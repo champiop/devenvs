7a42449290bc01d4c8a5a02993d0472416d6363a
Pierre Champion <pierre.champion.pro@gmail.com>
2026-08-21 09:23:08 +0200

    feat(base): Added nvim config

---
+++ base/nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,6 @@
+require("nvchad.configs.lspconfig").defaults()
+
+local servers = { "html", "cssls", "pyright", "clangd", "rust_analyzer", "jsonls", "tinymist" }
+vim.lsp.enable(servers)
+
+-- read :h vim.lsp.config for changing options of lsp servers 
